316f722a900dd63b070e42eb9f842dfd93669d23
unknown <cat.crusader31@gmail.com>
2023-03-03 23:37:09 +0200

    feat: add mouse selection for units
    add green material for terrain

--- code-wargame/Assets/Scripts/Player/ClientPlayerCameraScript.cs
+++ code-wargame/Assets/Scripts/Player/ClientPlayerCameraScript.cs
@@ -4,37 +4,5 @@ using UnityEngine;
 
 public class ClientPlayerCameraScript : MonoBehaviour
 {
-    [SerializeField]
-    Player player;
-
-    public Camera cam;
-
-
-    public GameObject unit;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-    public void RayCastFromCamera()
-    {
-        RaycastHit hit;
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out hit))
-        {
-            unit.GetComponent<Unit>().movement.MoveTo(hit.point);
-
-            // Do something with the object that was hit by the raycast.
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RayCastFromCamera();
-        }
-    }
+   
 }
---
+++ code-wargame/Assets/Scripts/Player/ClientPlayerControllerScript.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientPlayerControllerScript : MonoBehaviour
+{
+    [SerializeField]
+    Player player;
+
+    public Camera cam;
+
+    public Vector3 SelectBoxStartPos;
+    public Vector3 SelectBoxEndPos;
+    public Vector3 SelectBoxCenter;
+    public Vector3 SelectBoxSize;
+
+
+    public List<Unit> selected;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void SelectUnit()
+    {
+        selected = new List<Unit>();
+
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.tag == "Selectable")
+            {
+                selected.Add(hit.collider.gameObject.GetComponentInParent<Unit>());
+            }
+            else
+            {
+                selected = null;
+            }
+        }
+    }
+    public void SelectBoxStart()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out hit);
+        SelectBoxStartPos = hit.point;
+    }
+    public void SelectBoxEnd()
+    {
+        selected = new List<Unit>();
+
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out hit);
+        SelectBoxEndPos = hit.point;
+
+        SelectBoxCenter = (SelectBoxStartPos + SelectBoxEndPos) / 2;
+
+        SelectBoxSize = Abs((SelectBoxCenter - SelectBoxEndPos)*2);
+        
+        SelectBoxSize += new Vector3(0,1f,0);
+        Collider[] colliders = Physics.OverlapBox(SelectBoxCenter, SelectBoxSize);
+
+        foreach(Collider col in colliders)
+        {
+            if (col.tag== "Selectable")
+            {
+                selected.Add(col.gameObject.GetComponentInParent<Unit>());
+            }
+        }
+    }
+
+    public void OrderToMove()
+    {
+        if (selected == null) return;
+
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            foreach (Unit unit in selected)
+            {
+                unit.movement.MoveTo(hit.point);
+            }
+
+
+            // Do something with the object that was hit by the raycast.
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    SelectUnit();
+        //}
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SelectBoxStart();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            SelectBoxEnd();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            OrderToMove();
+        }
+
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(SelectBoxCenter,SelectBoxSize);
+    }
+    public Vector3 Abs(Vector3 v)
+    {
+        return new Vector3(Mathf.Abs(v.x), Mathf.Abs(v.y), Mathf.Abs(v.z));
+    }
+}
